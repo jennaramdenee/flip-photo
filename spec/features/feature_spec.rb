4cf97bc3c4ae0bcd6d677099ededc427e0633ed6
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-20 17:43:08 +0000

    Add tests to ensure that post can be created

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,10 +1,28 @@
 require "rails_helper"
 
 feature "Flipboard" do
+
   context "no posts have been added" do
     scenario "should display an empty page" do
       visit "/posts"
+      expect(page).to have_css("div#photos")
       expect(page).not_to have_css("div#photo-front")
     end
   end
+
+  context "add a new post" do
+    scenario "page should have a form to create a new post" do
+      visit "/posts/new"
+      expect(page).to have_css("form")
+    end
+
+    scenario "displays the new post once form has been completed" do
+      visit "/posts/new"
+      fill_in 'Date', with: '03/03/2017'
+      fill_in 'Description', with: "test"
+      click_button "Add Photo"
+      expect(page).to have_content("test")
+    end
+  end
+
 end

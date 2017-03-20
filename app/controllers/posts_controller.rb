a6c65752c43e84d3369e40b64454250f57d9ef6c
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-20 17:43:41 +0000

    Add create method to ensure new Post can be created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to '/posts'
+  end
+
+  def post_params
+    params.require(:post).permit(:date)
+    params.require(:post).permit(:description)
   end
 
 end

8e5e20f358fe09cd847247b1d84fc9511490b651
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-20 17:42:30 +0000

    Changed date datatype from date to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320142817) do
+ActiveRecord::Schema.define(version: 20170320170454) do
 
   create_table "posts", force: :cascade do |t|
-    t.date     "date"
+    t.text     "date"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false

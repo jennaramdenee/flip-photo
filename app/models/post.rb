05579c5c3432dcca7c4dd504b184fb7ce6c91dd5
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-21 22:51:16 +0000

    Enforce sizing of uploaded images to a specified dimension

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_attached_file :image, :styles => { :medium => "205x205#", thumb: "100x100>" }
+
+  # validates :image, presence: true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
